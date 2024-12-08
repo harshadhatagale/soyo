72f6c75eb72c925d746806bba5c251b519666857
harshadhatagale <harshadhatagale6@gmail.com>
2024-12-08 13:33:53 +0530

    login form

--- src/components/forms/LoginFormCust.jsx
+++ src/components/forms/LoginFormCust.jsx
@@ -85,7 +85,7 @@ const LoginFormCust = () => {
           </button>
         </form>
         <div className="text-center text-sm text-gray-600">
-          Don't have an account?{" "}
+        Don&apos;t have an account?{" "}
           <a
             href="#"
             className="font-medium text-indigo-600 hover:underline"
