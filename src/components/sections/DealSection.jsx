f3ba4d57125eee76b644521d23be8245b82953d2
harshadhatagale <harshadhatagale6@gmail.com>
2024-12-08 13:37:54 +0530

    login form

--- src/components/sections/DealSection.jsx
+++ src/components/sections/DealSection.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { FaFireAlt } from "react-icons/fa";
 export default function DealSection() {
     return (
-        <div className="lg:w-[80%] flex-col lg:flex-row space-x-5 h-16 my-6 mx-auto border border-gray-200 flex justify-center items-center rounded-md">
+        <div className="lg:w-[80%] flex-col lg:flex-row space-x-5 h-28 py-2 lg:h-16 my-6 mx-auto border border-gray-200 flex justify-center items-center rounded-md">
             <div className='flex justify-center items-center space-x-3 px-1 py-2'>
                 <div>
                     <img src="/fire.svg" alt="Fire" className='w-10 h-10'/>
